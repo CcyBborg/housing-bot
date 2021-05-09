7fa4d8eebdfa41cf9446f5430ec2bdde0cd9ae51
frokofaron <mineev96@gmail.com>
2021-05-10 00:15:20 +0300

    completed development backend

--- backend.py
+++ backend.py
@@ -86,6 +86,18 @@ def get_reg_count():
     return count
 
 
+def get_ident_key():
+    with open("key.txt", "r") as file:
+        return int(file.read().strip())
+
+
+def check_admin_rights(message):
+    return message.from_user.id == 221820979
+
+
+def check_back_to_start(message):
+    return message.text == "Вернуться в главное меню"
+
 
 def update_registered_data_id(message):
     query = (Registered.select())
@@ -102,3 +114,91 @@ def update_enrolled_id(tg_id, enrolled_id):
             user.enrolled_id = enrolled_id
             user.save()
 
+
+def check_user_enroll(message):
+    data = message.text.split(',')
+    if len(data) != 2:
+        return -2
+    data = [i.strip() for i in data]
+    query = (Enrolled.select())
+    for user in query:
+        if (str(user.surname)).lower() == data[0].lower() and (str(user.student_card)).lower() == data[1].lower():
+            update_enrolled_id(message.from_user.id, user.id)
+            return 0
+    return -1
+
+
+def add_series(message):
+    query = (Pass.select())
+    for user in query:
+        if user.id == message.from_user.id:
+            user.series = message.text
+            user.save()
+            return 0
+    Pass.create(
+        id=message.from_user.id,
+        series=message.text,
+        number=-1,
+        date_of_issue='01 января 1990 года',
+        issued_by='МВД РОССИИ',
+        place_of_registration='Россия'
+    )
+    return 0
+
+
+def add_number(message):
+    query = (Pass.select())
+    for user in query:
+        if user.id == message.from_user.id:
+            user.number = message.text
+            user.save()
+            return 0
+    return -1
+
+
+def add_date_of_issue(message):
+    query = (Pass.select())
+    for user in query:
+        if user.id == message.from_user.id:
+            user.date_of_issue = message.text
+            user.save()
+            return 0
+    return -1
+
+
+def add_issued_by(message):
+    query = (Pass.select())
+    for user in query:
+        if user.id == message.from_user.id:
+            user.issued_by = message.text
+            user.save()
+            return 0
+    return -1
+
+
+def add_place_of_registration(message):
+    query = (Pass.select())
+    for user in query:
+        if user.id == message.from_user.id:
+            user.place_of_registration = message.text
+            user.save()
+            return 0
+    return -1
+
+
+def check_for_key(message):
+    query = (Registered.select())
+    for user in query:
+        if user.tg_id == message.from_user.id:
+            if user.status == "stage_reg_fin":
+                return True
+    return False
+
+
+def check_for_set(message):
+    query = (Registered.select())
+    for user in query:
+        if user.tg_id == message.from_user.id:
+            if user.status == "stage_reg_set":
+                return True
+    return False
